297721878fce761a9361fa9a50b695408be86134
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-03-25 12:20:18 -0300

    atualizando codigo

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import express from "express";
 import cors from "cors";
 
 import PingController from "./controller/PingController.js";
-
+import './repository/connection.js';
 
 const servidor = express();
 servidor.use(cors());
 servidor.use(express.json());
 
 servidor.use(PingController);
+
 
 servidor.listen(process.env.PORT, () =>
   console.log(`API subiu com sucesso na porta ${process.env.PORT}`)
--- src/repository/connection.js
+++ src/repository/connection.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const conexao = await mysql.createConnection({
-  host: "127.0.0.1",
-  database: "catalogofilmesdb",
-  user: "root",
-  senha: "40673553",
+  host: process.env.HOST,
+  database: process.env.DB,
+  user: process.env.USER,
+  password: process.env.PWD,
 });
 
 console.log("BD conectado com sucesso!");
