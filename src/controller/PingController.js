1f2b86563875499eccba3bfdab364ddba85039ad
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-03-24 22:03:27 -0300

    criei um endpoint

---
+++ src/controller/PingController.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const endpoints = Router();
+
+endpoints.get("/ping", (req, resp) => {
+  resp.send("Pingg!!");
+});
+
+export default endpoints;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import PingController from "./controller/PingController.js";
+
 const servidor = express();
 servidor.use(cors());
 servidor.use(express.json());
+
+servidor.use(PingController);
 
 servidor.listen(process.env.PORT, () =>
   console.log(`API subiu com sucesso na porta ${process.env.PORT}`)
